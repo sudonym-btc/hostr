db23249b908ba7670a8d015c51d5482c781a0b45
pats2sats <5664617+pats2sats@users.noreply.github.com>
2025-03-07 12:40:01 -0600

    chore: added boltz

---
+++ docker/boltz/images/scripts/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -xe
+
+DIRECTORIES=(
+    "/root/.lightning-1"
+    "/root/.lightning-2"
+    "/root/.lnd-1"
+    "/root/.lnd-2"
+    "/root/.bitcoin"
+    "/root/.elements"
+    "/root/.boltz-client"
+    "/root/.boltz-backend"
+)
+
+cleanup() {
+    echo 'SIGINT or SIGTERM received, exiting'
+    exit 0
+}
+
+set_permissions() {
+    for dir in "${DIRECTORIES[@]}"; do
+        chmod -R 777 "$dir"
+    done
+}
+
+main() {
+    set_permissions
+    trap cleanup SIGINT SIGTERM
+    sleep infinity &
+    wait
+}
+
+main
