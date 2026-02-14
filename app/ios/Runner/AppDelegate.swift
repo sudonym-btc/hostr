d20919fd1d8cd53db58fb0e577811cd84098b900
pats2sats <5664617+pats2sats@users.noreply.github.com>
2026-02-14 04:58:41 -0600

    chore: updated dependencies

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -4,16 +4,12 @@ import GoogleMaps
 import workmanager_apple
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyBjcePUwkKwD-iMmHpjXVDV0MaiYH1dnGo")
-    GeneratedPluginRegistrant.register(with: self)
-    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
-      GeneratedPluginRegistrant.register(with: registry)
-    }
     WorkmanagerPlugin.registerPeriodicTask(
       withIdentifier: "com.sudonym.hostr.sync.ios.fetch",
       frequency: NSNumber(value: 15 * 60) // 20 minutes (15 min minimum)
@@ -25,5 +21,12 @@ import workmanager_apple
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
   }
 }
